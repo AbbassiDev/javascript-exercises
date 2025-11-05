85cc6c4e80c79b180187e4f049507268909914c7
Abbassi Dev <belalrabbassi@gmail.com>
2025-11-05 00:12:30 +0000

    Completed the repeatString exersize 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let repeated = '';
 
+    for (let i=0; i < num; i++) {
+        repeated += str;
+    }
+    
+    if (num < 0) return 'ERROR';
+    else return repeated
 };
+
+repeatString('hey', 3)
+
 
 // Do not edit below this line
 module.exports = repeatString;
