f267bb53af4312f0552c1108b7999be0eb7079a6
Abbassi Dev <belalrabbassi@gmail.com>
2025-11-09 23:20:28 +0000

    Completed excersize 8 removeFromArray. I learnt what the .filter method was and how I can use it.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(reversio) {
+    return reversio.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeMe) {
+    return arr.filter(item => !removeMe.includes(item));
 };
 
 // Do not edit below this line
