a7d33cb72856181445ac56355e1bab78de7e1cf1
Abbassi Dev <belalrabbassi@gmail.com>
2025-11-13 20:26:50 +0000

    Completed exercises 10 and 11.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return (year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0);
 };
+
+// attempt 1
+// if (year % 100 == 0 && !(year % 400 == 0)) {
+//     return false;
+//   } else {
+//     if (year % 4 == 0) {
+//       return true;
+//     } else return false
+//   }
 
 // Do not edit below this line
 module.exports = leapYears;
