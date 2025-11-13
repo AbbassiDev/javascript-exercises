e24cb761d9b7d8cd1897929dd093af61fabce134
Abbassi Dev <belalrabbassi@gmail.com>
2025-11-13 10:47:02 +0000

    Completed exercise 9 sumAll.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (
+        !Number.isInteger(num1) || 
+        !Number.isInteger(num2) ||
+        num1 < 0 ||
+        num2 < 0
+    ) {
+        return 'ERROR'
+    } 
 
+    smaller = Math.min(num1, num2)
+    larger = Math.max(num1, num2)
+
+    let total = 0;
+    for (let i = smaller; i <= larger; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
